32568d8fa23b497fb013f01b196887bc7c4bbe54
hujiachen <hujc@whut.com>
2023-09-08 14:40:54 +0800

    a gussing game betwin 10~99

--- test.cpp
+++ test.cpp
@@ -3,6 +3,35 @@ using namespace std;
 
 int main()
 {
-    cout << "This is my first cpp program in linux" << endl;
+    srand(time(NULL));
+    int ran = rand()%100;
+    int i =0;
+    int gus=0;
+    while (i<=10)
+    {
+        gus = 0;
+        cout << "输入一个在0~99之间的数" << endl;
+        cin >> gus;
+
+        if (gus>ran)
+            {
+            cout << "你猜大了！" << endl;
+            }
+        else if (gus<ran)
+        {
+            cout <<"你猜小了！" << endl;
+        }
+        else
+            {
+                cout <<"你才对了！" << endl;
+                i = 100;
+            }
+        i++;
+    };
+    if (i!=101)
+    {
+        cout << "你猜了10次都没有猜对!正确答案是 " << ran << endl;
+    }
+    
     return 0;
 }
